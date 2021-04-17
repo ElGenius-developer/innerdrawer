2f1e31bf8ebb79ff9d741825e02df67bb7c4c808
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2021-04-18 01:18:00 +0200

    Update inner_drawer.dart

--- lib/inner_drawer.dart
+++ lib/inner_drawer.dart
@@ -1,11 +1,7 @@
 library inner_drawer;
-import 'dart:async';
 import 'package:flutter/cupertino.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inner_drawer/inner_drawer.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 
