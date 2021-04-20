5544b13381cd7c99cb3401f2d05a3d2219c7296c
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2021-04-21 00:34:59 +0200

    v0.0.2

---
+++ lib/custom_appbar.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:inner_drawer/inner_drawer.dart';
+
+class MyCustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Size size;
+  final Color color;
+  final Color textColor;
+  final bool showDrawer;
+  final List<Widget> actions;
+  final String title;
+  final bool showTrailing;
+  const MyCustomAppBar({
+    Key key,
+    this.size,
+    this.showDrawer = false,
+    this.showTrailing = false,
+    @required this.title,
+    this.color,
+    this.actions,
+    this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = Colors.black;
+    /*his.textColor??(context.watch<ThemeCubit>().isDarkThemEnabled
+        ? Colors.white
+        : Colors.black);*/
+    return AppBar(
+      titleTextStyle: TextStyle(
+        fontFamily: 'Akaya',
+        fontSize: 32,
+      ),
+      systemOverlayStyle: Theme.of(context).appBarTheme.systemOverlayStyle,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      leading: IconButton(
+        icon: showDrawer
+            ? SvgPicture.asset(
+                'assets/icons/drawer.svg',
+                color: textColor,
+                fit: BoxFit.scaleDown,
+              )
+            : Icon(
+                Icons.arrow_back_ios_rounded,
+                color: textColor,
+                size: 35,
+              ),
+        onPressed: () => showDrawer
+            ? InnerCustomDrawer.innerDrawerKey.currentState.open()
+            : Navigator.pop(context),
+      ),
+      actions: actions ??
+          (showTrailing
+              ? [
+                  IconButton(
+                    icon: Icon(Icons.search, color: textColor, size: 35),
+                    onPressed: () {},
+                  ),
+                ]
+              : []),
+      backgroundColor: color ?? Colors.deepPurple,
+      // context.read<ThemeCubit>().defaultColor,
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Akaya',
+          color: textColor ?? Colors.white,
+        ),
+      ),
+
+      centerTitle: true,
+    );
+  }
+
+  @override
+  Size get preferredSize => size ?? Size(double.infinity, 60);
+}
--- lib/inner_drawer.dart
+++ lib/inner_drawer.dart
@@ -1,24 +1,29 @@
 library inner_drawer;
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inner_drawer/inner_drawer.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-
+part 'custom_appbar.dart';
 
 class InnerCustomDrawer extends StatelessWidget {
   final bool isRight;
   final Widget mainScreen;
   final Widget drawerContent;
-  static  GlobalKey<InnerDrawerState> innerDrawerKey =
-  GlobalKey<InnerDrawerState>();
+  static GlobalKey<InnerDrawerState> innerDrawerKey =
+      GlobalKey<InnerDrawerState>();
 
-  const InnerCustomDrawer({Key key, this.isRight = false, @required this.mainScreen, this.drawerContent})
+  const InnerCustomDrawer(
+      {Key key,
+      this.isRight = false,
+      @required this.mainScreen,
+      this.drawerContent})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InnerDrawer(
-
       key: innerDrawerKey,
       borderRadius: 28,
       offset: IDOffset.only(
@@ -27,11 +32,9 @@ class InnerCustomDrawer extends StatelessWidget {
       onTapClose: true,
       leftAnimationType: InnerDrawerAnimation.quadratic,
       rightAnimationType: InnerDrawerAnimation.quadratic,
-      backgroundDecoration: BoxDecoration(
-
-        color: Colors.deepPurple
-        // color: Theme.of(context).scaffoldBackgroundColor,
-      ),
+      backgroundDecoration: BoxDecoration(color: Colors.deepPurple
+          // color: Theme.of(context).scaffoldBackgroundColor,
+          ),
       swipe: false,
       swipeChild: false,
       proportionalChildArea: false,
